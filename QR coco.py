849f9f08e06fae72f528ed0c0346dd0a41eae36e
Sam <32861928+Daradris@users.noreply.github.com>
2021-02-17 17:23:34 +0100

    First push

---
+++ QR coco.py
@@ -0,0 +1,172 @@
+from pygame import mixer
+import cv2
+import datetime
+from library_manager import Library_manager
+from imutils.video import VideoStream
+from pyzbar import pyzbar
+import imutils
+
+import time
+
+if __name__ == '__main__':
+    #BLOCK 1
+    #setting the library
+    filepath = """H:\Music"""
+    music_lib = Library_manager(filepath)
+    music_lib.read_library()
+
+
+    # QR CODE  
+    # initialize the video stream and allow the camera sensor to warm up
+    print("[INFO] starting video stream...")
+    vs = VideoStream(src=0).start()
+    # vs = VideoStream(usePiCamera=True).start()
+    time.sleep(2.0)
+
+
+    previous_order = 'A 0'
+    time_of_first_blanck = datetime.datetime.now()
+    time_of_last_oder = datetime.datetime.now()
+
+
+    # MUSIC
+    mixer.init()
+    music_sound = mixer.Channel(0)
+    system_sound = mixer.Channel(1)
+    system_sound.play(mixer.Sound("beep.mp3"))
+
+    pause_state = False
+    previous_songs = []
+    current_song = ''
+    next_songs = []
+    playlist_mode = False
+
+    while True:
+        # QR CODE
+        frame = vs.read()
+        # find the barcodes in the frame and decode each of the barcodes
+        barcodes = pyzbar.decode(frame)
+        print (barcodes)
+        if barcodes:
+            data = barcodes[0].data.decode("utf-8")
+        else:
+            data = ''
+        new_order = 'A 0'
+        if data:
+            if data != previous_order:
+                
+                new_order = data
+                previous_order = new_order
+            time_of_last_oder = datetime.datetime.now()
+        if data == "":
+            time_of_first_blanck = datetime.datetime.now()
+            if (time_of_first_blanck - time_of_last_oder ) > datetime.timedelta(seconds=5):
+                previous_order = 'A 0'
+
+        previous_songs = previous_songs[0:5]
+        isplaying = music_sound.get_busy()
+
+
+        if new_order == 'A 1':# play/ unpause
+            if current_song != '':
+                if pause_state == True:
+                    system_sound.play(mixer.Sound("beep.mp3"))
+                    music_sound.unpause()
+                    pause_state = False
+            pass
+
+        elif new_order == 'A 2':# PAUSE
+            if current_song != '':
+                if pause_state == False:
+                    music_sound.pause()
+                    system_sound.play(mixer.Sound("beep.mp3"))
+                    pause_state = True
+                pass
+
+        elif new_order == 'A 3': # STOP
+            if current_song != '':
+                if pause_state == False:
+                    music_sound.pause()
+                    system_sound.play(mixer.Sound("beep.mp3"))
+            pause_state = False
+            previous_songs = []
+            current_song = ''
+            next_songs = []
+            playlist_mode = False
+
+        elif new_order == 'A 4':# previous
+            if previous_songs:
+                if current_song:
+                    next_songs.insert(0, current_song)
+                    previous_songs[0] = current_song
+                    previous_songs.pop(0)
+                    system_sound.play(mixer.Sound("beep.mp3"))
+                    music_sound.play(mixer.Sound(current_song))
+                else:
+                    current_song = previous_songs[0]
+                    previous_songs.pop(0)
+                    system_sound.play(mixer.Sound("beep.mp3"))
+                    music_sound.play(mixer.Sound(current_song))
+            else:
+                system_sound.play(mixer.Sound("beep.mp3"))
+                music_sound.play(mixer.Sound(current_song))
+            pass
+
+        elif new_order == 'A 5':# next
+            if playlist_mode == True:
+                if current_song:
+                    if next_songs:
+                        previous_songs.insert(0, current_song)
+                        current_song = next_songs[0]
+                        next_songs.pop(0)
+                        system_sound.play(mixer.Sound("beep.mp3"))
+                        music_sound.play(mixer.Sound(current_song))
+            pass
+
+        elif new_order == 'A 6': #playlist mode ON
+            if playlist_mode == False:
+                playlist_mode = True
+                system_sound.play(mixer.Sound("beep.mp3"))
+
+
+        elif new_order == 'A 7':
+            #feeling lucky
+            pass
+
+        elif new_order != 'A 0': # PLAY MUSIC 
+            song_to_play = music_lib.find_music_path_from_library(new_order)
+            print (song_to_play)
+            if song_to_play:
+                if playlist_mode == False:
+                    
+                    if current_song:
+                        previous_songs.insert(0, current_song)
+                    current_song = song_to_play
+
+                    system_sound.play(mixer.Sound("beep.mp3"))
+                    music_sound.play(mixer.Sound(song_to_play))
+
+                if playlist_mode == True:
+                    
+                    next_songs.append(song_to_play)
+                    system_sound.play(mixer.Sound("beep.mp3"))
+                pass
+
+        isplaying = music_sound.get_busy()
+
+        if isplaying == 0 : # NOT PLAYING
+            if next_songs:
+                current_song = next_songs[0]
+                next_songs.pop(0)
+                music_sound.play(mixer.Sound(current_song))
+            else:
+                current_song = ''
+
+    # free camera object and exit
+    print("[INFO] cleaning up...")
+    csv.close()
+    cv2.destroyAllWindows()
+    vs.stop()
+
+
+
---
+++ library_manager.py
@@ -0,0 +1,50 @@
+import os
+import json
+# hash : song _ data : track number title album Artist 
+
+import hashlib
+
+from mutagen.mp3 import MP3  
+from mutagen.easyid3 import EasyID3  
+
+
+class Library_manager:
+    HASH_MAX_LENGTH = 10
+    def __init__(self, filepath):
+        self.library_filepath = filepath 
+        
+        self.music_files = None
+
+    def library(self):
+        self.music_files = {}
+        for root, _, files in os.walk(self.library_filepath):
+            for file in files:
+                if file.endswith(".mp3"):
+                    music_filepath = os.path.join(root, file)
+                    song_info = MP3(music_filepath, ID3=EasyID3)
+                    stringf = "{0} - {1} - {2} - {3}".format(
+                        song_info['tracknumber'][0],
+                        song_info['title'][0],
+                        song_info['album'][0],
+                        song_info['artist'][0]
+                    )
+                    hasheds = str(self.song_hash(stringf))
+                    self.music_files[hasheds] = music_filepath
+        with open("data/library.json", 'w', encoding ='utf8') as json_file: 
+            json.dump(self.music_files, json_file, ensure_ascii = False, indent=4) 
+
+    def song_hash(self, song_info):
+        hash_object = hashlib.md5(song_info.encode())
+        return hash_object.hexdigest()[0:self.HASH_MAX_LENGTH]
+
+    def read_library(self):
+        open_file = open("data/library.json",) 
+        self.music_files = json.load(open_file) 
+        open_file.close()
+
+    def find_music_path_from_library(self, hashcode):
+        return self.music_files.get(hashcode, '')
+if __name__ == '__main__':
+    filepath = """H:\Music"""
+    lib = Library_manager(filepath)
+    lib.library()
